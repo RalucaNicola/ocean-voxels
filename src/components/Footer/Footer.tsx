96c855d4dd5aeb1def6c3949eb70d67d1b3a8277
Raluca Nicola <RNicola@esri.com>
2023-01-20 15:17:21 +0100

    Add more info in footer and option to toggle intro

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleIntroScreen } from '../../store/Map/reducer';
+import { selectIntroScreenEnabled } from '../../store/Map/selectors';
 import * as styles from './Footer.module.css';
 
 const Footer = () => {
+  const introScreenEnabled = useSelector(selectIntroScreenEnabled);
+  const dispatch = useDispatch();
   return (
     <footer className={styles.appFooter}>
       <p className={styles.esriLogo}>
@@ -8,6 +13,61 @@ const Footer = () => {
           <img src='./assets/esri_science_of_where_white.png'></img>
         </a>
       </p>
+      {introScreenEnabled && (
+        <p className={styles.attribution}>
+          Data attribution{' '}
+          <a href='https://www.usgs.gov/' target='_blank'>
+            USGS
+          </a>
+          <span>|</span>
+          <a href='https://www.noaa.gov/' target='_blank'>
+            NOAA
+          </a>
+          <span>|</span>
+          <a href='https://www.nasa.gov/' target='_blank'>
+            NASA
+          </a>
+          <span>|</span>
+          <a href='https://www.fws.gov/' target='_blank'>
+            U.S. Fish and Wildlife Service
+          </a>
+          <span>|</span>
+          <a href='https://niwa.co.nz/' target='_blank'>
+            NIWA
+          </a>
+          <span>|</span>
+          <a href='https://www.natureserve.org/' target='_blank'>
+            NatureServe
+          </a>
+          <span>|</span>
+          <a href='https://mgel.env.duke.edu/' target='_blank'>
+            Duke Marine Geospatial Ecology Lab
+          </a>
+          <span>|</span>
+          <a href='https://www.grida.no/' target='_blank'>
+            Grid Arendal
+          </a>
+          <span>|</span>
+          <a href='https://www.whoi.edu/' target='_blank'>
+            Woods Hole Oceanographic Institution
+          </a>
+          <br></br>
+          Built with{' '}
+          <a href='https://developers.arcgis.com/javascript/latest/' target='_blank'>
+            ArcGIS Maps SDK for JavaScript
+          </a>
+        </p>
+      )}
+      {!introScreenEnabled && (
+        <button
+          className={styles.showIntroButton}
+          onClick={() => {
+            dispatch(toggleIntroScreen());
+          }}
+        >
+          Show intro
+        </button>
+      )}
     </footer>
   );
 };
