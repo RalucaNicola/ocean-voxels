3ce37faa3fcd191ca0c5eac5adeabb4ed964fd53
Raluca Nicola <RNicola@esri.com>
2023-01-27 10:52:15 +0100

    Add mobile menu button

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -2,10 +2,24 @@ import * as styles from './Title.module.css';
 
 const Title = () => {
   return (
-    <div className={styles.title}>
-      <h1>
-        <span className={styles.capitalLetter}>O</span>cean <span className={styles.capitalLetter}>V</span>oxels
-      </h1>
+    <div className={styles.titleContainer}>
+      <div className={styles.title}>
+        <h1>
+          <span className={styles.capitalLetter}>O</span>cean <span className={styles.capitalLetter}>V</span>oxels
+        </h1>
+      </div>
+      <button
+        onClick={() => {
+          console.log('menu button');
+        }}
+        className={styles.mobileMenuButton}
+      >
+        <svg width='50px' height='25px'>
+          <line className={styles.menuLine} x1='1.5' y1='1.5' x2='36.5' y2='1.5' />
+          <line className={styles.menuLine} x1='1.5' y1='12.5' x2='36.5' y2='12.5' />
+          <line className={styles.menuLine} x1='1.5' y1='23.5' x2='36.5' y2='23.5' />
+        </svg>
+      </button>
     </div>
   );
 };
