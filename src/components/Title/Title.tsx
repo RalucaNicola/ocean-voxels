1b47e8cd8c9a7b5517132b4649ff312e9983281a
Raluca Nicola <ralucagnicola@gmail.com>
2023-07-06 22:17:38 +0200

    Update Title.tsx

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -5,8 +5,8 @@ const Title = () => {
     <div className={styles.titleContainer}>
       <div className={styles.title}>
         <h1>
-          <span className={styles.capitalLetter}>E</span>cological <span className={styles.capitalLetter}>M</span>arine{' '}
-          <span className={styles.capitalLetter}>U</span>nits
+          <span className={styles.capitalLetter}>3D</span> <span className={styles.capitalLetter}>O</span>cean{' '}
+          <span className={styles.capitalLetter}>E</span>xplorer
         </h1>
       </div>
     </div>
